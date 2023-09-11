fdbb924af8a5564df5fd5dd0c7f74848b2b365b4
Machative <machjayus@gmail.com>
2023-09-11 14:34:51 -0600

    Added temperature enum,wroter burner class methods and attributes - Colton

--- src/Burner.java
+++ src/Burner.java
@@ -1,4 +1,67 @@
 
 public class Burner {
 	
+	public enum Temperature{
+		BLAZING,HOT,WARM,COLD;
+		
+	}
+	
+	private Temperature myTemperature;
+	private Setting mySetting;
+	private int timer;
+	public final int TIME_DURATION = 2;
+	
+	
+	
+	public Temperature getTemperature() {
+		return myTemperature;
+	}
+	
+	public Burner() {
+		myTemperature = Temperature.COLD;
+		mySetting = Setting.OFF;	
+		
+	}
+	
+	public void plusButton() {
+		timer = TIME_DURATION;
+		switch(mySetting) {
+			case OFF:
+				mySetting = Setting.LOW;
+			case LOW:
+				mySetting = Setting.MEDIUM;
+			case MEDIUM:
+				mySetting = Setting.HIGH;
+			case HIGH:
+				break;	
+		}
+	}
+	
+	public void minusButton() {
+		timer = TIME_DURATION;
+		switch(mySetting) {
+			case OFF:
+				break;
+			case LOW:
+				mySetting = Setting.OFF;
+			case MEDIUM:
+				mySetting = Setting.LOW;
+			case HIGH:
+				mySetting = Setting.MEDIUM;
+		}
+	}
+	
+	public void updateTemperature(){
+		
+	}
+	
+	public void display() {
+		System.out.println(mySetting.toString());
+	}
+	
+	
+	
+	
+	
+	
 }
